009d4daf722d1d4a7c2cb1fc2c914ff721f2317b
Aluno2 <Aluno2>
2016-05-30 15:40:38 -0300

    Inicial

---
+++ gen/br/org/ort/velniasalkanas/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package br.org.ort.velniasalkanas;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/br/org/ort/velniasalkanas/CadastroActivity.java
@@ -0,0 +1,13 @@
+package br.org.ort.velniasalkanas;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class CadastroActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cadastro);
+	}
+}
